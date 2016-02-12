39051ca66525b80b8127b18908f6d1f9b80f251e
Michal Orzechowski <orzechowski.michal@gmail.com>
2016-02-12 12:50:26 +0100

    added docker commands to build.sh

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-gitbook install
-gitbook build
+docker run -t --rm -v `pwd`:/docs -i  docker.onedata.org/gitbook-compiler:1.0.0 install
+docker run -t --rm -v `pwd`:/docs -i  docker.onedata.org/gitbook-compiler:1.0.0 build
 mv _book build_dir
