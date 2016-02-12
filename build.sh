e4179d10824f3da15aa66926c2649b901655874a
Lukasz Dutka <lukasz.dutka@gmail.com>
2016-02-12 13:53:03 +0100

    No move of results

--- build.sh
+++ build.sh
@@ -2,4 +2,3 @@
 cd gitbook
 docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.0 install
 docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.0 build
-mv _book ../build_dir
