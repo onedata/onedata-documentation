23e2e5ac4b2256bc3e469be76f9a5c2348ae5c66
Lukasz Dutka <lukasz.dutka@gmail.com>
2016-02-12 13:45:14 +0100

    No tty for bamboo

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd gitbook
-docker run --rm -v `pwd`:/docs -i  docker.onedata.org/gitbook-compiler:1.0.0 install
-docker run --rm -v `pwd`:/docs -i  docker.onedata.org/gitbook-compiler:1.0.0 build
+docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.0 install
+docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.0 build
 mv _book ../build_dir
