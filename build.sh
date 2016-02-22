1fd8744aae9837cc082a6ca59805dd4c9fa67395
Michal Orzechowski <orzechowski.michal@gmail.com>
2016-02-22 14:37:31 +0000

    updated docker-gitbook-compiler version to 1.1.0

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.1 install
-docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.1 build
+docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.1.0 install
+docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.1.0 build
