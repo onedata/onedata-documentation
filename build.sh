c6320aaa1d0e06df733604ba89811544706d4383
Michal Orzechoski <michal.orzechowski@cyfronet.pl>
2016-02-22 13:22:51 +0000

    updated docker-gitbook-compiler version to fix permission problem

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.0 install
-docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.0 build
+docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.1 install
+docker run --rm -v `pwd`:/docs docker.onedata.org/gitbook-compiler:1.0.1 build
