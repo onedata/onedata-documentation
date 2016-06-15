ce108d4c7f1b7f486e50698d3efded0022a66098
Bartosz Kryza <bkryza@gmail.com>
2016-06-15 15:19:23 +0200

    Updated structure for direct Swagger integration

---
+++ update_toc.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+cat SUMMARY.md.template > SUMMARY.md
+sed -i -e  '/SWAGGER_INCLUDE ONEZONE_PATHS/ {' -e 'r doc/advanced/rest/onezone/PATHS_TOC.md' -e 'd' -e '}' SUMMARY.md
+sed -i -e  '/SWAGGER_INCLUDE ONEZONE_DEFINITIONS/ {' -e 'r doc/advanced/rest/onezone/DEFINITIONS_TOC.md' -e 'd' -e '}' SUMMARY.md
+sed -i -e  '/SWAGGER_INCLUDE ONEPROVIDER_PATHS/ {' -e 'r doc/advanced/rest/oneprovider/PATHS_TOC.md' -e 'd' -e '}' SUMMARY.md
+sed -i -e  '/SWAGGER_INCLUDE ONEPROVIDER_DEFINITIONS/ {' -e 'r doc/advanced/rest/oneprovider/DEFINITIONS_TOC.md' -e 'd' -e '}' SUMMARY.md
+
