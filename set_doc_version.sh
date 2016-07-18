12e71a3eff17912aafbc4f3a1cd81acfe5228d5b
Michal Orzechowski <orzechowski.michal@gmail.com>
2016-07-18 13:01:28 +0000

    added docker-compose variable "ONEDATA_DOC_VERSION:" wich is passed to documentation and displayed in the corner

--- set_doc_version.sh
+++ set_doc_version.sh
@@ -1,3 +1,13 @@
-#!/bin/bash
+#!/bin/sh
+if [ $# -eq 0 ] ; then
+  file="_book/gitbook/plugins/gitbook-plugin-onedata-theme/onedata.js"
+else 
+  file="$1"
+fi
 
-sed -i "s/ONEDATA_DOC_VERSION = .*;/ONEDATA_DOC_VERSION = \"$1\";/g" node_modules/gitbook-plugin-onedata-theme/book/onedata.js
+if [[ "$ONEDATA_DOC_VERSION" != "" ]]; then
+  echo "$ONEDATA_DOC_VERSION" | sed -e '/^\s*$/d' -e 's/^[[:space:]]*/"/g' -e 's/$/\\n"+/' | sed -e '$ s/.$//' | tr -d '\n' > /tmp/eee1
+  file=$1
+  sed -i -r -e '/window.ONEDATA_DOC_VERSION = null;/ {'  -e 's/(.* =).*/\1 /'  -e 'r /tmp/eee1'  -e 'a ;' -e '}' $file
+  head $file
+fi
