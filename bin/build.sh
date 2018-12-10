a166b5257b6a87396e9ad856563b188289c2db1c
Bartek Kryza <bkryza@gmail.com>
2018-12-10 19:40:48 +0100

    Fixed build on Bamboo without TTY

--- bin/build.sh
+++ bin/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
-docker run --rm -it -v `pwd`:/docs -v gitbook_cache:/root/.npm docker.onedata.org/gitbook-compiler:ID-a2746a377f install
-docker run --rm -it -v `pwd`:/docs -v gitbook_cache:/root/.npm docker.onedata.org/gitbook-compiler:ID-a2746a377f build
-docker run --rm -it -v `pwd`:/docs onedata/redoc-cli:v1 sh -c "cd /docs/_book/doc/swagger; /docs/bin/redoc_build_recursive.sh"
+test -t 1 && USE_TTY="-t"
+
+docker run --rm -i $USE_TTY -v `pwd`:/docs -v gitbook_cache:/root/.npm docker.onedata.org/gitbook-compiler:ID-a2746a377f install
+docker run --rm -i $USE_TTY -v `pwd`:/docs -v gitbook_cache:/root/.npm docker.onedata.org/gitbook-compiler:ID-a2746a377f build
+docker run --rm -i $USE_TTY -v `pwd`:/docs onedata/redoc-cli:v1 sh -c "cd /docs/_book/doc/swagger; /docs/bin/redoc_build_recursive.sh"
