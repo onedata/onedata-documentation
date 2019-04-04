fc3206717ee40070376d01916ccf77d45ec937c5
Jakub Liput <jakub.liput@gmail.com>
2019-04-04 11:22:44 +0200

    VFS-5323 Fixing scroll on mobile styles problem

--- bin/build.sh
+++ bin/build.sh
@@ -4,6 +4,6 @@ GITBOOK_COMPILER_IMAGE=docker.onedata.org/gitbook-compiler:ID-a2746a377f
 
 test -t 1 && USE_TTY="-t"
 
-docker run --rm -i $USE_TTY -v `pwd`:/docs -v gitbook_cache:/root/.npm $GITBOOK_COMPILER_IMAGE install
-docker run --rm -i $USE_TTY -v `pwd`:/docs -v gitbook_cache:/root/.npm $GITBOOK_COMPILER_IMAGE build
+docker run --rm -i $USE_TTY -v `pwd`:/docs -v gitbook_cache:/root/.npm $GITBOOK_COMPILER_IMAGE install || exit $?
+docker run --rm -i $USE_TTY -v `pwd`:/docs -v gitbook_cache:/root/.npm $GITBOOK_COMPILER_IMAGE build || exit $?
 docker run --rm -i $USE_TTY -v `pwd`:/docs onedata/redoc-cli:v1 sh -c "cd /docs/_book/doc/swagger; /docs/bin/redoc_build_recursive.sh"
