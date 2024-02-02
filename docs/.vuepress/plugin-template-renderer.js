824a02e85b367f2856240ccd6db027962ff583b3
Jakub Liput <jakub.liput@gmail.com>
2024-02-02 08:44:20 +0100

    VFS-11179 Added file comments

--- docs/.vuepress/plugin-template-renderer.js
+++ docs/.vuepress/plugin-template-renderer.js
@@ -1,3 +1,18 @@
+/**
+ * Invokes template renderer before VuePress build starts (both `build` and `dev`).
+ * See README.md in this project for more information about template system.
+ * 
+ * We cannot use `ready` hook because VuePress needs files generated before it starts -
+ * otherwise it will crash because of lack of pages in the index.
+ * 
+ * Currently this plugin does not support livereload re-rendering because `updated` hook
+ * seems to not work properly (from our tests: it does not work at all) in VuePress 1.9.9.
+ *
+ * @author Jakub Liput
+ * @copyright (C) 2024 ACK CYFRONET AGH
+ * @license This software is released under the MIT license cited in 'LICENSE.txt'.
+ */
+
 const process = require('process');
 const TemplateRenderer = require('./template-renderer');
 
