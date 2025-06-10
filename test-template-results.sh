3d838ce04224f0ab54c4dca685242f5ceda123cf
Jakub Liput <jakub.liput@gmail.com>
2025-06-10 09:17:54 +0200

    VFS-12831 Added test-template-results

---
+++ test-template-results.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Authors: Jakub Liput
+# Copyright (C) 2024 ACK CYFRONET AGH
+# This software is released under the MIT license cited in 'LICENSE.txt'
+
+# This script checks if current contents in current branch of template results in Markdown
+# files are generated from the current contents of templates and partials. The repository
+# must be clean before perfoming test, because git is used for checking the diff.
+
+cd "$(dirname "$0")"
+
+if [ -z "$(git status --porcelain)" ]; then 
+  make render-templates
+  if [ -z "$(git status --porcelain)" ]; then 
+    echo "All templates have generated fresh results."
+    exit 0
+  else
+    echo "There are some template results that are not compliant with current templates "
+    echo "or partials. Use 'make render-templates' and commit changes."
+    # git reset --hard
+    exit 1
+  fi
+else 
+  echo "The repository must be clean before using this script."
+  exit 2
+fi
