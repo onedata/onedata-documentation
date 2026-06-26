199302219832d037cc3803d75a53fd4d507679f7
Jakub Liput <jakub.liput@gmail.com>
2026-06-26 12:27:53 +0200

    VFS-13668 Added OnedataRESTFSSpec to sidebar

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -56,6 +56,7 @@ module.exports = {
               '/user-guide/interfaces/oneclient',
               '/user-guide/interfaces/onedata-fs',
               '/user-guide/interfaces/onedata-rest-fs',
+              '/user-guide/interfaces/onedata-rest-fsspec',
               '/user-guide/interfaces/onedata-file-rest-client',
               // '/user-guide/interfaces/s3',
               '/user-guide/interfaces/data-access-rest-api',
